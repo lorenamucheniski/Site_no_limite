09adfe8a21e7986f55a6f4e7ca6a21b00de69101
Pedro Henrique <pedro2266809ph@gmail.com>
2024-01-04 21:47:43 -0300

    feat: add nomes por js no footer

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,2 +1,8 @@
-
-
+const lestras = document.querySelector('.nomes');
+let arrayDeNomes = ["Pedro Henrique", "Lorena Mucheniski"];
+        
+function mostrasDadosNaTela(nomes) {
+    lestras.innerHTML = "Feito por: " + nomes[0] + " e " + nomes[1] + " ©";
+}
+    
+mostrasDadosNaTela(arrayDeNomes);
